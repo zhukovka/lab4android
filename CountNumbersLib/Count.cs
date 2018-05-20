d3e7a4f15c7346084c030fcd5bc34538ba9c964c
abush <abush@PUNKOR-PC>
2018-05-20 21:44:39 +0300

    android

---
+++ CountNumbersLib/Count.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace CountNumbersLib
+{
+    public class Count
+    {
+        public int CountWords(string input)
+        {
+            return input.Split().Length;
+        }
+
+        public int CountSpaces(string input)
+        {
+            return input.Count(Char.IsWhiteSpace);
+        }
+
+        public double CountSum(int a1, int an)
+        {
+          
+            int n = an - a1 + 1;
+            return (a1 + an) / 2.0 * n;
+        }
+    }
+}
--- Lab4Android/Lab4Android/MainPage.xaml.cs
+++ Lab4Android/Lab4Android/MainPage.xaml.cs
@@ -1,17 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using CountNumbersLib;
+using System;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Xamarin.Forms;
 
 namespace Lab4Android
 {
-	public partial class MainPage : ContentPage
-	{
-		public MainPage()
-		{
-			InitializeComponent();
-		}
-	}
+    public partial class MainPage : ContentPage
+    {
+        double result;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void OnCalculate(object sender, EventArgs e)
+        {
+            Count countLib = new Count();
+            int countSpaces = countLib.CountSpaces(input.Text);
+            int countWords = countLib.CountWords(input.Text);
+            int a1 = Math.Min(countSpaces, countWords);
+            int an = Math.Max(countSpaces, countWords);
+            
+            result = countLib.CountSum(a1, an);
+            output.Text = $"There are {countWords} words and {countSpaces} spaces in the string.\n" +
+                $"The sum of numbers between {a1} and {an} is {result}";
+        }
+
+    }
 }
